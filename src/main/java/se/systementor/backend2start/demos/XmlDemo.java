b13fa1cf51063fbf15f51d016597b4103f17477d
stefan <stefan.holmberg@systementor.se>
2024-05-06 12:44:37 +0200

    0506

--- src/main/java/se/systementor/backend2start/demos/XmlDemo.java
+++ src/main/java/se/systementor/backend2start/demos/XmlDemo.java
@@ -1,6 +1,18 @@
 package se.systementor.backend2start.demos;
 
 
+// VAD ÄR PROBLEMET?
+// - vi har en textmassa i XML
+// - vi är ju Javaprogrammerare - OBJEKT
+
+// hämta en xml FIL från en "annan sajt"
+
+// rename class
+// autogenerate classes  https://json2csharp.com/code-converters/xml-to-java
+
+//Hur vet Jackson att vi vill använda book classen vid mappningen? Jag kan inte se att den nämns i själva mappninskoden.
+
+
 import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.springframework.boot.CommandLineRunner;
@@ -12,17 +24,46 @@ import java.net.URL;
 public class XmlDemo implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
+//        JacksonXmlModule module = new JacksonXmlModule();
+//        module.setDefaultUseWrapper(false);
+//        XmlMapper xmlMapper = new XmlMapper(module);
+//        catalog theBooks = xmlMapper.readValue(new URL("https://axmjqhyyjpat.objectstorage.eu-amsterdam-1.oci.customer-oci.com/n/axmjqhyyjpat/b/aspcodeprod/o/books.xml"),
+//                catalog.class
+//        );
+//
+//        for( book s : theBooks.books ){
+//            System.out.println(s.title);
+//            System.out.println(s.id);
+//        }
+
+
+//
         JacksonXmlModule module = new JacksonXmlModule();
         module.setDefaultUseWrapper(false);
         XmlMapper xmlMapper = new XmlMapper(module);
-        students theStudents = xmlMapper.readValue(new URL("https://axmjqhyyjpat.objectstorage.eu-amsterdam-1.oci.customer-oci.com/n/axmjqhyyjpat/b/externalbucket/o/data-1vdvx7Um7UzezAhpKyNmQ.xml"),
-                students.class
-                );
+        Catalog theBooks = xmlMapper.readValue(new URL("https://axmjqhyyjpat.objectstorage.eu-amsterdam-1.oci.customer-oci.com/n/axmjqhyyjpat/b/aspcodeprod/o/books2.xml"),
+                Catalog.class
+        );
 
-        for( student s : theStudents.student ){
-            System.out.println(s.name);
-            System.out.println(s.country);
+        for( book s : theBooks.books ){
+            System.out.println(s.title);
+            System.out.println(s.id);
         }
+
+
+
+
+//        JacksonXmlModule module = new JacksonXmlModule();
+//        module.setDefaultUseWrapper(false);
+//        XmlMapper xmlMapper = new XmlMapper(module);
+//        students theStudents = xmlMapper.readValue(new URL("https://axmjqhyyjpat.objectstorage.eu-amsterdam-1.oci.customer-oci.com/n/axmjqhyyjpat/b/externalbucket/o/data-1vdvx7Um7UzezAhpKyNmQ.xml"),
+//                students.class
+//                );
+//
+//        for( student s : theStudents.student ){
+//            System.out.println(s.name);
+//            System.out.println(s.country);
+//        }
 
     }
 }
---
+++ src/main/java/se/systementor/backend2start/demos/book.java
@@ -0,0 +1,25 @@
+package se.systementor.backend2start.demos;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlText;
+
+import java.util.Date;
+
+public class book {
+    @JacksonXmlProperty(isAttribute = true)
+    public String id;
+
+    public String author;
+    public String title;
+
+    @JacksonXmlProperty(localName = "genre")
+    public String category;
+
+    //public float price;
+    public price price;
+    @JacksonXmlProperty(localName = "publish_date")
+    public Date publishDate;
+
+    public String description;
+}
+
