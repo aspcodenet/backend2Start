9d6ee35400defd9da9ac22143db3cb724f2d5daf
stefan <stefan.holmberg@systementor.se>
2024-05-12 18:56:10 +0200

    Merge remote-tracking branch 'origin/main'

--- src/test/java/se/systementor/backend2start/services/books/BookServiceTest.java
+++ src/test/java/se/systementor/backend2start/services/books/BookServiceTest.java
@@ -7,6 +7,8 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import se.systementor.backend2start.demos.book;
 
 import java.io.IOException;
--- src/test/java/se/systementor/backend2start/services/books/BookServiceTestIT.java
+++ src/test/java/se/systementor/backend2start/services/books/BookServiceTestIT.java
@@ -1,28 +1,17 @@
 package se.systementor.backend2start.services.books;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
-import se.systementor.backend2start.demos.book;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
-@Profile("IntegrationTest")
-class BookServiceIntegrationTest {
+class BookServiceTestIT {
     @Autowired
     BookService sut;
 
@@ -46,7 +35,6 @@ class BookServiceIntegrationTest {
         assertTrue(  result.contains("</publish_date>") );
         assertTrue(  result.contains("<description>") );
         assertTrue(  result.contains("</description>") );
-
 
     }
 
